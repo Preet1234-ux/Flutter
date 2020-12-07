dd8e3b813d3ed1e983881ec7f6fb5effe510c6a5
Preet Chaudhary <62163447+Preet1234-ux@users.noreply.github.com>
2020-12-07 16:24:48 +0530

    Add files via upload

---
+++ Search_Image/lib/main.dart
@@ -0,0 +1,11 @@
+
+import 'package:backendconnection/screens/imagelist.dart';
+import 'package:flutter/material.dart';
+void main(){
+
+runApp(MaterialApp(
+    title:'Calling API',
+  debugShowCheckedModeBanner: false,
+  home:SearchList(),
+));
+}
